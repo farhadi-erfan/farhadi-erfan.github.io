f854e311855a40922702d58bf55f26d39802a8f1
farhadi-erfan <farhadi.erfan76@gmail.com>
2026-05-15 06:29:57 +0000

    Deploying to gh-pages from @ farhadi-erfan/farhadi-erfan.github.io@bdd06a288a498059385cb2d78ee9e1024b711bbe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,16 +461,6 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "projects-intro-gd",
-          title: 'Intro-GD',
-          description: "Godot add-on for tutorials and onboarding sequences",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/intro-gd/";
-            },},{id: "projects-watching-others-sleep",
-          title: 'Watching Others Sleep',
-          description: "Short film, writer and director",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/watching-others-sleep/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
