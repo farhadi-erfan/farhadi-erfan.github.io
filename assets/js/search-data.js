7bc56d499c178a794170ca2b5e091bd6e48f6be1
farhadi-erfan <farhadi.erfan76@gmail.com>
2026-05-15 06:29:29 +0000

    Deploying to gh-pages from @ farhadi-erfan/farhadi-erfan.github.io@3e3a8859ba61f3991760d0672d0e4660f3e5b49f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,6 +461,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-intro-gd",
+          title: 'Intro-GD',
+          description: "Godot add-on for tutorials and onboarding sequences",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/intro-gd/";
             },},{id: "projects-watching-others-sleep",
           title: 'Watching Others Sleep',
           description: "Short film, writer and director",
