060fabfd4178367beacda79f3ef2f073a1df53eb
farhadi-erfan <farhadi.erfan76@gmail.com>
2026-05-15 06:10:51 +0000

    Deploying to gh-pages from @ farhadi-erfan/farhadi-erfan.github.io@3fc057bd674fbef3d2dd0b8e926861951fb383c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -496,6 +496,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-beetrap-mc",
+          title: 'BeeTrap-MC',
+          description: "Minecraft-based learning tool about filter bubbles",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/beetrap-mc/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
